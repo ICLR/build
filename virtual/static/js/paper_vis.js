0cb4ec0e1f27657b6ac086998f2fcaa6eb85ebb6
Alex Rush <sasha.rush@gmail.com>
2020-04-20 16:18:27 -0400

    .

--- virtual/static/js/paper_vis.js
+++ virtual/static/js/paper_vis.js
@@ -11,6 +11,8 @@ const filters = {
     title: null
 };
 
+const summaryBy = 'keywords' // or: "abstract"
+
 let currentTippy = null;
 let brush = null;
 
@@ -25,12 +27,11 @@ const sel_papers = d3.select('#sel_papers');
 
 const plot_size = () => {
     const cont = document.getElementById('container');
-    // console.log(window.innerHeight-100, cont.offsetWidth,"--- window.innerWidth, cont.offsetWidth");
     const wh = Math.max(window.innerHeight - 280, 300)
     let ww = Math.max(cont.offsetWidth - 210, 300)
-    if (cont.offsetWidth<768)  ww = cont.offsetWidth-10.0;
+    if (cont.offsetWidth < 768) ww = cont.offsetWidth - 10.0;
 
-    if ( (wh / ww > 1.3)) {
+    if ((wh / ww > 1.3)) {
         const min = Math.min(wh, ww)
         return [min, min]
     } else {
@@ -78,19 +79,29 @@ function brush_ended() {
     let parts = null;
     let count = 0;
     all_sel.forEach(paper => {
-        parts = paper.content.abstract.split(/[.]?\s+/)
-        parts.forEach(p => {
-            if (p.length < 3) return;
-            p = p.toLowerCase();
-            count = words_abstract.get(p) | 0;
-            count += 1;
-            words_abstract.set(p, count);
-        })
+        if (summaryBy === 'keywords') {
+            paper.content.keywords.forEach(kw => {
+                count = words_abstract.get(kw) | 0;
+                count += 1;
+                words_abstract.set(kw, count);
+            })
+        } else {
+            parts = paper.content.abstract.split(/[.]?\s+/)
+            parts.forEach(p => {
+                if (p.length < 3) return;
+                p = p.toLowerCase();
+                count = words_abstract.get(p) | 0;
+                count += 1;
+                words_abstract.set(p, count);
+            })
+        }
+
+
     })
     stopwords.forEach(sw => words_abstract.delete(sw));
     const abstract_words = [...words_abstract.entries()]
       .sort((a, b) => -a[1] + b[1])
-      .slice(0, 20);
+      .slice(0, 15);
 
     if (abstract_words.length > 0) {
         explain_text_plot.style('display', 'none');
@@ -176,14 +187,16 @@ const updateVis = () => {
     if (!currentTippy) {
         currentTippy = tippy('.dot', {
             content(reference) {
-                // const id = reference.getAttribute('data-template');
-                // const template = document.getElementById(id);
-                // console.log(reference,"--- reference");
-                // console.log(d3.select(reference), "--- d3.select(reference)");
-                return tooltip_template(d3.select(reference).datum());
+                return d3.select(reference).datum().content.title;
+                // return tooltip_template(d3.select(reference).datum());
+            },
+            onShow(instance){
+                const d = d3.select(instance.reference).datum()
+                instance.setContent(tooltip_template(d))
             },
             allowHTML: true
         });
+
     }
 
 }
@@ -264,7 +277,7 @@ const start = () => {
 
 const updateFilterSelectionBtn = value => {
     d3.selectAll('.filter_option label')
-      .classed('active', function(){
+      .classed('active', function () {
           const v = d3.select(this).select('input').property('value')
           return v === value;
       })
